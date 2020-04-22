31be1a80b9be25618d806a7a0fa5dfe694c4bb9a
Scott Howes <howes_sm@hotmail.com>
2020-04-22 12:49:40 -0700

    changed requirements.txt to include pyinstaller and added build.sh to create .exe file

---
+++ build.sh
@@ -0,0 +1,5 @@
+"env/Scripts/pyinstaller.exe" --onefile -i "media/icon.png" --name="Logical Clock" --paths="env/Lib/site-packages/PyQt5/Qt/bin" LogicalClock.py
+#cp "dist/Crack Detection Test Environment" .
+#rm -r build
+#rm -r dist
+#rm "Crack Detection Test Environment.spec"
